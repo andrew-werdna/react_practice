106f618b9803f777c684ad1c3cfd4566b6e43de1
Andrew Brown <andrew.brown5717@gmail.com>
2017-08-30 16:41:29 -0500

    added in styling for hover of user cards and added alt to image

--- src/Components/UserCard/UserCard.js
+++ src/Components/UserCard/UserCard.js
@@ -6,7 +6,7 @@ class UserCard extends Component {
     return (
       <div className="user-card">
         <div className="user-card-image">
-          <img src={this.props.data.image.length ? this.props.data.image.length : "images/profile.png" } />
+          <img alt={this.props.data.name} src={this.props.data.image.length ? this.props.data.image.length : "images/profile.png" } />
         </div>
         <div className="user-card-details">
           <div className="user-card-name">
