19457a262dad04efb613c7fb118df15fcb2694a6
Andrew Brown <andrew.brown5717@gmail.com>
2017-08-30 20:15:54 -0500

    updated UserCard to have redirect and click handler

--- src/Components/UserCard/UserCard.js
+++ src/Components/UserCard/UserCard.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import './UserCard.css';
+import { Redirect } from 'react-router-dom';
 
 class UserCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false,
+      link: this.props.api.users.slice(window.location.origin.length) + `/${this.props.data.id}`
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    let nState = Object.assign({}, this.state);
+    nState.redirect = true;
+    this.setState(nState);
+  }
   render() {
+    if ( this.state.redirect ) {
+      return (
+        <Redirect to={`${this.state.link}`} />
+      );
+    }
     return (
-      <div className="user-card">
+      <div onClick={this.handleClick} className="user-card">
         <div className="user-card-image">
           <img alt={this.props.data.name} src={this.props.data.image.length ? this.props.data.image.length : "images/profile.png" } />
         </div>
