47e70f1fd653a9c44f405af8290164a6661b439b
Andrew Brown <andrew.brown5717@gmail.com>
2017-08-30 20:15:07 -0500

    updated User component to retrieve API data and render it

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    };
+    this.getUser = this.getUser.bind(this);
+  }
+  componentDidMount() {
+    this.getUser();
+  }
+  getUser() {
+    let uri = `${this.props.api.users}/${this.props.match.params.id}`;
+    console.log(uri);
+    axios.get(uri)
+      .then(_user => {
+        console.log(_user);
+        this.setState({
+          user: _user.data
+        });
+      });
+  }
   render() {
     return (
-      <div>{JSON.stringify(this.props.data)}</div>
+      <div>{JSON.stringify(this.state.user)}</div>
     );
   }
 }
