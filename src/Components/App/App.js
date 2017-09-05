5b5eb9d9a72f8f7f1c23f89967386d20869e5b43
Andrew Brown <andrew.brown5717@gmail.com>
2017-09-05 11:09:51 -0500

    added in link to all posts in nav

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -24,7 +24,7 @@ class App extends Component {
             <ul>
               <li><Link to="/">Home</Link></li>
               <li><Link to="/users">Users</Link></li>
-              {/* <li><Link to="/posts">Posts</Link></li> */}
+              <li><Link to="/posts">Posts</Link></li>
             </ul>
           </nav>
           
