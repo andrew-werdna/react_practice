048e03dbe0653add5e24be2e4fc4b2f190e7c949
Andrew Brown <andrew.brown5717@gmail.com>
2017-09-05 11:11:21 -0500

    updated post list to allow navigation directly to the user who made the post or the post itself

--- src/Components/PostList/PostList.js
+++ src/Components/PostList/PostList.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './PostList.css';
+import { Redirect } from 'react-router-dom';
 
 class PostList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       posts: [],
-      loading: true
+      loading: true,
+      userRedirect: false,
+      userRedirectLink: '',
+      postRedirect: false,
+      postRedirectLink: ''
     };
     this.getPosts = this.getPosts.bind(this);
+    this.handlePostClick = this.handlePostClick.bind(this);
+    this.handleUserClick = this.handleUserClick.bind(this);
+    this.formatLink = this.formatLink.bind(this);
   }
   componentDidMount() {
     this.getPosts();
+  }
+  formatLink(link) {
+    return link.slice(window.origin.length);
+  }
+  handleUserClick(id) {
+    let nState = Object.assign({}, this.state);
+    let link = this.formatLink(this.props.api.users);
+    nState.userRedirect = true;
+    nState.userRedirectLink = `${link}/${id}`;
+    this.setState(nState);
+  }
+  handlePostClick(id) {
+    let nState = Object.assign({}, this.state);
+    let link = this.formatLink(this.props.api.posts);
+    nState.postRedirect = true;
+    nState.postRedirectLink = `${link}/${id}`;
+    this.setState(nState);
   }
   getPosts() {
     let uri;
@@ -31,6 +56,16 @@ class PostList extends Component {
       });
   }
   render() {
+    if ( this.state.userRedirect ) {
+      return (
+        <Redirect to={`${this.state.userRedirectLink}`} />
+      );
+    }
+    else if ( this.state.postRedirect ) {
+      return (
+        <Redirect to={`${this.state.postRedirectLink}`} />
+      );
+    }
     return (
       <div className="post-list">
         {
@@ -38,10 +73,10 @@ class PostList extends Component {
             let k = `${post.title}_${post.id}`;
             return (
               <div key={k} className="post-container">
-                <div className="userId">
+                <div onClick={() => this.handleUserClick(post.userId)} className="userId">
                   <span>User ID:</span> {post.userId}
                 </div>
-                <div className="post-title">
+                <div onClick={() => this.handlePostClick(post.id)} className="post-title">
                   <h2>{post.title}</h2>
                 </div>
                 <div className="post-body">
